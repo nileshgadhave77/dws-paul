40e0f54cb22fdd8bf1ec85f5020892e38d392a9d
nileshgadhave77 <nileshgadhave@gmail.com>
2024-06-11 21:59:34 +0530

    Update FundTransferServiceImpl.java

--- challenge/src/main/java/com/dws/challenge/Service/FundTransferServiceImpl.java
+++ challenge/src/main/java/com/dws/challenge/Service/FundTransferServiceImpl.java
@@ -1,6 +1,7 @@
 package com.dws.challenge.Service;
 
 import com.dws.challenge.Entity.FundTransferEntity;
+import com.dws.challenge.Exception.AmountNotMatchException;
 import com.dws.challenge.Repository.FundTransferRepository;
 import com.dws.challenge.model.dto.request.FundTransfer;
 import com.dws.challenge.model.dto.response.FundTransferResponse;
@@ -8,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Slf4j
@@ -18,8 +20,13 @@ public class FundTransferServiceImpl implements  FundTransferService{
 
 
     @Override
-    public FundTransferResponse transferFund(FundTransfer request) {
+    public FundTransferResponse transferFund(FundTransfer request) throws AmountNotMatchException {
         log.info("Sending fund transfer request {}" + request.toString());
+
+        if(request.getAmount().compareTo(BigDecimal.ZERO)<0)
+        {
+            throw new AmountNotMatchException(" Amount should be greater than 0 ");
+        }
         FundTransferEntity entity = new FundTransferEntity();
         BeanUtils.copyProperties(request, entity);
         entity.setStatus(TransactionStatus.PENDING);
